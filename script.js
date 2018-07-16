e85e90cd11dd8509aff96cead3f1328b9a5cb2ca
RytisDr <r.drazdauskas@gmail.com>
2018-07-16 23:56:32 +0200

    Styles and links up mobile contact page.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 "use strict"
+let navContact = document.querySelector(".contactTriangle");
+
+navContact.addEventListener('click', GoToContact)
+
+function GoToContact(){
+    document.querySelector(".index").classList.toggle("inContactPage");
+    document.querySelector("#indexContent").classList.toggle("dontDisplay");
+    document.querySelector(".contactPage").classList.toggle("dontDisplay");
+    document.querySelector(".contactTriangle h2:nth-child(2)").classList.toggle("dontDisplay");
+    document.querySelector(".contactTriangle h2").classList.toggle("dontDisplay");
+    navContact.classList.toggle("homeTriangle");
+}
