877134d8b0fc522882ea9a30084bd4d834b83e3b
RytisDr <r.drazdauskas@gmail.com>
2018-07-18 23:31:19 +0200

    .htaccess now includes .html and index removal from url. Contact page color swithed, triangle text also changed color. JS: added if statement to index functions.

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 "use strict"
-let navContact = document.querySelector(".contactTriangle");
 
-navContact.addEventListener('click', GoToContact)
+/*INDEX SCRIPT*/
+if (window.location.pathname.includes("index")) {
+    let navContact = document.querySelector(".contactTriangle");
 
-function GoToContact(){
-    document.querySelector(".index").classList.toggle("inContactPage");
-    document.querySelector("#indexContent").classList.toggle("dontDisplay");
-    document.querySelector(".contactPage").classList.toggle("dontDisplay");
-    document.querySelector(".contactTriangle h2:nth-child(2)").classList.toggle("dontDisplay");
-    document.querySelector(".contactTriangle h2").classList.toggle("dontDisplay");
-    navContact.classList.toggle("homeTriangle");
+    navContact.addEventListener('click', GoToContact)
+
+    function GoToContact() {
+        document.querySelector(".index").classList.toggle("inContactPage");
+        document.querySelector(".indexContent").classList.toggle("dontDisplay");
+        document.querySelector(".contactPage").classList.toggle("dontDisplay");
+        document.querySelector(".contactTriangle h2:nth-child(2)").classList.toggle("dontDisplay");
+        document.querySelector(".contactTriangle h2").classList.toggle("dontDisplay");
+        navContact.classList.toggle("homeTriangle");
+        document.querySelector(".worksTriangle h2").classList.toggle("fontColorSwitch");
+    }
 }
